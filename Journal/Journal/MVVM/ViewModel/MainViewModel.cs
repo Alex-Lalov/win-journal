33cbfd013ff5d009f20d0ac12d757df625fbab53
ZJAC240 <lalovalex@gmail.com>
2022-04-08 20:31:58 +0100

    Created the GUI, using Binding and Views

---
+++ Journal/Journal/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Journal.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand EntriesViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+        public EntriesViewModel EntriesVM { get; set; } 
+
+        private object currentView;
+
+        public object CurrentView
+        {
+            get { return this.currentView; }
+            set {
+                this.currentView = value; 
+                OnPropertyChanged(); 
+            }
+            
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            EntriesVM = new EntriesViewModel();
+            CurrentView = HomeVM;
+
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            EntriesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = EntriesVM;
+            });
+        }
+    }
+}
